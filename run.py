8715e6066a5edc60ad67654c984c812b967610d6
Madi <72963376+Madi-S@users.noreply.github.com>
2021-03-11 15:27:06 +0600

    added new gui

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 from kivy.app import App
+
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
+
 from kivy.properties import ObjectProperty
 
 
@@ -59,7 +62,8 @@ class MyApp(App):
     def build(self):
         # return Label(text='Shaiken Madi\'s First Kivy App')
         # return MyGrid()
-        return MyWidget()
+        # return MyWidget()
+        return FloatLayout()
 
 
 if __name__ == '__main__':
